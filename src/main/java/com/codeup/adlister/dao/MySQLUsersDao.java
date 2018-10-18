a7b97a53bac160f8828ada4fb98ba1bf3c5416d8
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-10-18 16:02:21 -0500

    Completed passwords exercise

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -2,11 +2,14 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MySQLUsersDao implements Users {
-    private Connection connection;
+    private static Connection connection;
 
     public MySQLUsersDao(Config config) {
         try {
@@ -51,7 +54,7 @@ public class MySQLUsersDao implements Users {
         }
     }
 
-    private User extractUser(ResultSet rs) throws SQLException {
+    private static User extractUser(ResultSet rs) throws SQLException {
         if (! rs.next()) {
             return null;
         }
@@ -62,5 +65,37 @@ public class MySQLUsersDao implements Users {
             rs.getString("password")
         );
     }
+
+//    private static void fixPW (User user) {
+//        String hashedPW = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+//        String query = String.format("UPDATE users SET password = %s WHERE username = %s", hashedPW, user.getUsername());
+//
+//        try {
+//            PreparedStatement stmt = connection.prepareStatement(query);
+//            stmt.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error creating new user", e);
+//        }
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        Config config = new Config();
+//        Users dao = new MySQLUsersDao(config);
+//        String sql = "SELECT * FROM users";
+//        try {
+//            Statement stmt = connection.createStatement();
+//            ResultSet rs = stmt.executeQuery(sql);
+//            while (rs.next()) {
+//                fixPW(extractUser(rs));
+//            }
+//        }
+//        catch (SQLException e) {
+//            throw new RuntimeException("Error creating new user", e);
+//        }
+//
+//
+//    }
 
 }
